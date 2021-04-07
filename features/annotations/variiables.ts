7abd005c92154492da36ff7479378abfdc792a9d
Chris Castellano <c.castellano18@gmail.com>
2021-04-07 13:27:09 -0500

    more variables

---
+++ features/annotations/variiables.ts
@@ -0,0 +1,31 @@
+let apples: number = 5
+//number is the annotation. assigning number to apples. Any other value will be error
+
+let speed: string = 'fast'
+let hasName: boolean = true;
+
+let nothingMuch: null = null
+let nothing: undefined = undefined
+
+//built in objects
+let now: Date = new Date()
+
+//array
+let colors: string[] = ['red', 'green', 'blue']
+let myNumbers: number[] = [1,2,3]
+let truths: boolean[] = [true, true, false]
+
+//Classes
+class Car{
+
+} 
+
+let car: Car = new Car();
+
+//Object literal
+let point: {x: number; y: number} = {
+  x: 10, 
+  y: 20
+}
+
+//
