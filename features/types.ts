b6728d87ff3eecf964190f91a53aa06f41b0b88f
Chris Castellano <c.castellano18@gmail.com>
2021-04-07 13:17:44 -0500

    annotations/inferences

---
+++ features/types.ts
@@ -0,0 +1,13 @@
+const today = new Date()
+//typing today. will bring up a menu of functions that today can use because today has been defined as a Date, so it'll show date functions
+today.getMonth()
+
+const person = {
+  age: 20
+}
+
+class Color{
+
+}
+const red = new Color()
+//red would have access to any properties created in the Color class
