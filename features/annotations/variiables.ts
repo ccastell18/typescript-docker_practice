75ab8bf27031f15ec4b991ade0f3019412c2e9bc
Chris Castellano <c.castellano18@gmail.com>
2021-04-07 22:56:21 -0500

    type annotations

--- features/annotations/variiables.ts
+++ features/annotations/variiables.ts
@@ -35,3 +35,29 @@ const logNumber: (i: number) => void = (i: number) =>{
 }
 
 //if all annotations are deleted, the computer will use inference to assign value to variables
+
+//When to use Annotations
+//1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates:{x: number; y: number} = JSON.parse(json)
+console.log(coordinates)
+
+//2)declare on one line and initialize on another
+let words = ['red', 'green', 'blue']
+let foundWord: boolean;
+
+for(let i = 0; i < words.length; i++){
+  if (words[i] === 'green'){
+    foundWord = true
+  }
+}
+
+//3) variable whose type cannot be inferred correctly
+let number = [-10, -1, -20]
+let numberAboveZero: boolean | number = false;
+
+for(let i = 0; i < number.length; i++){
+  if(number[i] > 0){
+    numberAboveZero = number[i]
+  }
+}
