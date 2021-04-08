4e28db12cc87be876c07d2a9b3bebde3dac0d50e
Chris Castellano <c.castellano18@gmail.com>
2021-04-07 23:12:54 -0500

    functions

---
+++ features/annotations/functions.ts
@@ -0,0 +1,29 @@
+
+//basic function annotation. Arguments need the annotation and the annotation after the number represents what is returned from the function
+const add = (a: number,b: number ): number =>{
+  return a+b 
+
+}
+
+//This is wrong because there is no return annotation, so the return inference will seem right when there is an error
+const subtract = (a: number, b: number) =>{
+  a-b
+}
+
+function divide (a: number, b: number): number{
+  return a/b
+}
+
+const multiply = function(a: number, b: number): number{
+  return a * b
+}
+
+//void can be null or undefined
+const logger = (message: string): void =>{
+  console.log(message)
+}
+
+//Throws error so the function will never finish
+const throwError = (message: string): never => {
+  throw new Error(message)
+}
