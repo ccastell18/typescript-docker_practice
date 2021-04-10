8a179ea90a60956555135a3f6f457a4ccfa1acdc
Chris Castellano <c.castellano18@gmail.com>
2021-04-10 13:01:11 -0500

    classes

--- features/annotations/classes.ts
+++ features/annotations/classes.ts
@@ -1,13 +1,22 @@
 class Vehicle {
+  //automatically runs if instance of class is used
+  constructor(public color: string){}
+
   
   protected honk(): void{
     console.log('beep')
   }
 }
 
+const car2 = new Vehicle('orange')
+console.log(car2.color)
+
 //a child class because it extends Vehicle
 //drive is overwritten
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string){
+    super('red')
+  }
   private drive(): void{
     console.log('vroom')
   }
@@ -18,6 +27,6 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car()
+const car = new Car(4, 'red')
 
 car.startDriving()
