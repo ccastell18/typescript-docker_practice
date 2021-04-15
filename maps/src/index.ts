801967e05097b2ca173043a5c49aed31090fed0a
Chris Castellano <c.castellano18@gmail.com>
2021-04-14 20:24:29 -0500

    user marker

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -2,4 +2,7 @@ import {User} from './User'
 import {Company} from './Company'
 import {CustomMap} from './CustomMap'
 
-new CustomMap("map")
+const customMap = new CustomMap("map")
+const user = new User();
+
+customMap.addUserMarker(user)
